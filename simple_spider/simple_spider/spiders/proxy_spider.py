60205afd1e9d0bcc1808c0abfccecac4b5a32006
Pavel Zubko <tyyrok@gmail.com>
2024-09-19 22:16:25 +0400

    add main functionality: getting proxy data and trying to send proxy data

--- simple_spider/simple_spider/spiders/proxy_spider.py
+++ simple_spider/simple_spider/spiders/proxy_spider.py
@@ -1,14 +1,22 @@
 import base64
-from typing import Any, Iterable
+import json
+from datetime import datetime, UTC
+from typing import Any, Iterable, Self
 
-import scrapy
+from scrapy import signals, Spider
+from scrapy.crawler import Crawler
 from scrapy.http import Response, Request
 
 URL_FOR_SCRAPING = "http://free-proxy.cz/en/"
+URL_FOR_SENDING_DATA = "https://test-rg8.ddns.net/task"
+URL_FOR_GETTING_FORM_TOKEN = "https://test-rg8.ddns.net/api/get_token"
+URL_FOR_SENDING_RESULT = "https://test-rg8.ddns.net/api/post_proxies"
 LIMIT_PAGES_TO_PARSE = 5
+LIMIT_VALUES_TO_SEND = 9
+TOKEN = "t_7e7ea5ef"
 
 
-class ProxySpider(scrapy.Spider):
+class ProxySpider(Spider):
     name = "proxy"
     parsed_proxies = []
     max_retries = 2
@@ -22,12 +30,18 @@ class ProxySpider(scrapy.Spider):
 
     def __init__(self, name: str | None = None, **kwargs: Any):
         super().__init__(name, **kwargs)
-        self.pages_parsed = 0
+        self.pages_parsed = 1
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler, *args, **kwargs) -> Self:
+        spider = super(ProxySpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
 
     def start_requests(self) -> Iterable[Request]:
         if self.proxies:
             proxy = self.proxies.pop()
-            yield scrapy.Request(
+            yield Request(
                 url=URL_FOR_SCRAPING,
                 callback=self.parse,
                 errback=self.error_handle,
@@ -63,10 +77,10 @@ class ProxySpider(scrapy.Spider):
                 decoded_ip = script_text.split('"')[1]
                 ip = base64.b64decode(decoded_ip).decode("utf-8")
                 port = elem.xpath('td[2]/span/text()').get()
-                yield {
-                    "ip": ip,
-                    "port": port
-                }
+                #yield {
+                #    "ip": ip,
+                #    "port": port
+                #}
                 self.parsed_proxies.append(f"{ip}:{port}")
         self.pages_parsed += 1
         if self.pages_parsed <= LIMIT_PAGES_TO_PARSE:
@@ -81,3 +95,67 @@ class ProxySpider(scrapy.Spider):
                     'retry_count': response.meta.get('retry_count', 0)
                 },
             )
+        else:
+            yield Request(
+                url=URL_FOR_SENDING_DATA, 
+                method='GET',
+                callback=self.get_token
+            )
+    """
+    def send_callback(self, reason):
+        self.crawler.engine.crawl(
+            Request(
+                url=URL_FOR_SENDING_DATA, 
+                method='GET',
+                callback=self.get_token
+            )
+        )
+    """
+    def get_token(self, reason):
+        yield Request(
+            url=URL_FOR_GETTING_FORM_TOKEN,
+            callback=self.send_data
+        )
+
+    def send_data(self, response: Response):
+        response_cookie = response.headers.get('Set-Cookie')
+        form_token = response_cookie.decode("utf-8").split(";")[0]
+        cookies = {
+            "form_token": form_token.split("=")[1],
+            "x-user_id": "t_7e7ea5ef"
+        }
+        proxy_chunks = [
+            self.parsed_proxies[i: i + LIMIT_VALUES_TO_SEND]
+            for i in range(0, len(self.parsed_proxies), LIMIT_VALUES_TO_SEND)
+        ]
+        for chunk in proxy_chunks:
+            data = self.prepare_data(chunk)
+            yield Request(
+                url=URL_FOR_SENDING_RESULT,
+                method='POST',
+                body=json.dumps(data),
+                cookies=cookies,
+                headers={'Content-Type': 'application/json'},
+                callback=self.after_submission,
+                cb_kwargs=dict(proxy_chunk=chunk)
+            )
+
+    def prepare_data(self, proxies: list[str]) -> dict:
+        return {
+            "user_id": TOKEN,
+            "len": len(proxies),
+            "proxies": ", ".join(proxies)
+        }
+
+    def after_submission(self, response: Response, proxy_chunk: list):
+        save_id = response.json()["save_id"]
+        yield {
+            save_id: proxy_chunk
+        }
+
+    def spider_closed(self, spider, reason):
+        start_time = self.crawler.stats.get_value('start_time')
+        finish_time = datetime.now(UTC)
+        spent_time = str(finish_time - start_time).split(".")[0]
+        with open("time.txt", "w") as f:
+            f.write(f"{spent_time}\n")
---
+++ simple_spider/simple_spider/spiders/test_spider.py
@@ -0,0 +1,60 @@
+import json
+from datetime import datetime, UTC
+from typing import Self
+
+import scrapy
+from scrapy import signals
+from scrapy.crawler import Crawler
+from scrapy.http import Request, Response
+
+class MySpider(scrapy.Spider):
+    name = "send"
+    start_urls = ["https://test-rg8.ddns.net/task"]
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler, *args, **kwargs) -> Self:
+        spider = super(MySpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        return spider
+
+    def spider_closed(self, spider, reason):
+        start_time = self.crawler.stats.get_value('start_time')
+        finish_time = datetime.now(UTC)
+        spent_time = str(finish_time - start_time).split(".")[0]
+        with open("time.txt", "w") as f:
+            f.write(f"{spent_time}\n")
+
+    def parse(self, response: Response):
+        # Delay processing to wait for JavaScript updates
+        yield Request(
+            url="https://test-rg8.ddns.net/api/get_token",  # URL where you need to submit data
+            method='GET',
+            callback=self.post_request
+        )
+
+
+    def post_request(self, response):
+        response_cookie = response.headers.get('Set-Cookie')
+        form_token = response_cookie.decode("utf-8").split(";")[0]
+        cookies = {
+            "form_token": form_token.split("=")[1],
+            "x-user_id": "t_7e7ea5ef"
+        }
+        print("FUCK ", cookies)
+        data = {"user_id": "t_7e7ea5ef", "len": 3, "proxies": "129.213.89.36:80, 129.213.89.36:80, 129.213.89.36:80"}
+        yield Request(
+            url="https://test-rg8.ddns.net/api/post_proxies",  # URL where you need to submit data
+            method='POST',
+            body=json.dumps(data),
+            cookies=cookies,
+            headers={'Content-Type': 'application/json'},
+            callback=self.after_submission,
+            cb_kwargs=dict(proxy_chunk=[data["proxies"]])
+        )
+        
+
+    def after_submission(self, response: Response, proxy_chunk: list):
+        save_id = response.json()["save_id"]
+        yield {
+            save_id: proxy_chunk
+        }
